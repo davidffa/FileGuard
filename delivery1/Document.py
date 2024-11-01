3666fe8afc82223d33b3c1f974fe37631448c35f
BirrasOBacano <birras13@sapo.pt>
2024-11-01 17:08:57 +0000

    Project startup with class

---
+++ delivery1/Document.py
@@ -0,0 +1,9 @@
+import Permissions_Consts
+
+class Document:
+    def __init__(self, name, file, metadata):
+        self.name = name
+        self.metadata = metadata
+        self.file = file
+
+ 
