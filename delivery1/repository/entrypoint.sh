7d90d77617a7ed9bbd805ba1b81fc7243037abd6
David Amorim <davidfilipeamorim@gmail.com>
2024-11-16 14:31:01 +0000

    Encrypt metadata with master key

--- delivery1/repository/entrypoint.sh
+++ delivery1/repository/entrypoint.sh
@@ -4,4 +4,4 @@ flask db init
 flask db migrate
 flask db upgrade
 
-flask run -h 0.0.0.0 -p 8000
+python3 app.py $MASTER_PASSWORD
--- delivery1/repository/src/crypto.py
+++ delivery1/repository/src/crypto.py
@@ -1,6 +1,29 @@
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.padding import PKCS7
+
+
+def pbkdf2(password: str, length: int, salt: bytes):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=length,
+        salt=salt,
+        iterations=480000
+    )
+
+    return kdf.derive(bytes(password, "utf8"))
+
+def encrypt_aes256_cbc(plaintext: bytes, secret_key: bytes, iv: bytes) -> bytes:
+    algorithm = algorithms.AES256(secret_key)
+
+    padder = PKCS7(algorithm.block_size).padder()
+    padded_data = padder.update(plaintext) + padder.finalize()
+
+    cipher = Cipher(algorithm, modes.CBC(iv))
+    encryptor = cipher.encryptor()
+
+    return encryptor.update(padded_data) + encryptor.finalize()
 
 
 def decrypt_aes256_cbc(secret_key: bytes, iv: bytes, ciphertext: bytes) -> bytes:
